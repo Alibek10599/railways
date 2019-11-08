2e157b8f191011292dd2e35a00c7c7798f4c4efc
Altynbek Nurtaza <n.altynbek.kz@gmail.com>
2019-11-08 18:44:37 +0600

    second commit

--- src/main/java/com/hooli/railways/controller/DemoController.java
+++ src/main/java/com/hooli/railways/controller/DemoController.java
@@ -17,6 +17,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.ArrayList;
 import java.util.List;
